7d2c6d0e990b0abc231f09dc6798d1f7f120562c
tiddledeveloper <intern@tiddle.app>
2026-02-03 08:34:39 +0400

    lintered files

--- src/widget.py
+++ src/widget.py
@@ -3,16 +3,16 @@ def mask_account_card(acc_info: str) -> str:
         slice_type = "Maestro"
         slice_1 = acc_info[8:12]
         slice_2 = acc_info[12:14]
-        slice_2 = slice_2 + '**'
-        slice_3 = '****'
+        slice_2 = slice_2 + "**"
+        slice_3 = "****"
         slice_4 = acc_info[20:24]
         return f"{slice_type} {slice_1} {slice_2} {slice_3} {slice_4}"
     elif acc_info[0:10] == "MasterCard":
         slice_type = "MasterCard"
         slice_1 = acc_info[11:15]
         slice_2 = acc_info[15:17]
-        slice_2 = slice_2 + '**'
-        slice_3 = '****'
+        slice_2 = slice_2 + "**"
+        slice_3 = "****"
         slice_4 = acc_info[23:]
         return f"{slice_type} {slice_1} {slice_2} {slice_3} {slice_4}"
     elif acc_info[0:12] == "Visa Classic":
@@ -23,6 +23,7 @@ def mask_account_card(acc_info: str) -> str:
         return acc_info[10:]
     else:
         return f"Счет **{acc_info[-4:]}"
+
 
 def get_date(date_old: str) -> str:
     slice_year = date_old[0:4]
