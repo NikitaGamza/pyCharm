b5e867503bd04c4b67538310368f5b12e33ac421
NikitaGamza <gamza-ns@mail.ru>
2026-02-07 18:33:16 +0400

    fix: reusable functions

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from src.widget import get_date, mask_account_card
 
 get_mask_card_number(2222222222222222)
 get_mask_account(22222222222222221234)
-print(mask_account_card("MasterCard 7158300734726758"))
+print(mask_account_card("Maestro 7158300734726758"))
 print(get_date("2024-03-11T02:26:18.671407"))
--- src/widget.py
+++ src/widget.py
@@ -1,28 +1,15 @@
+from src.masks import get_mask_account, get_mask_card_number
+
+
 def mask_account_card(acc_info: str) -> str:
-    if acc_info[0:7] == "Maestro":
-        slice_type = "Maestro"
-        slice_1 = acc_info[8:12]
-        slice_2 = acc_info[12:14]
-        slice_2 = slice_2 + "**"
-        slice_3 = "****"
-        slice_4 = acc_info[20:24]
-        return f"{slice_type} {slice_1} {slice_2} {slice_3} {slice_4}"
-    elif acc_info[0:10] == "MasterCard":
-        slice_type = "MasterCard"
-        slice_1 = acc_info[11:15]
-        slice_2 = acc_info[15:17]
-        slice_2 = slice_2 + "**"
-        slice_3 = "****"
-        slice_4 = acc_info[23:]
-        return f"{slice_type} {slice_1} {slice_2} {slice_3} {slice_4}"
-    elif acc_info[0:12] == "Visa Classic":
-        return acc_info[13:]
-    elif acc_info[0:13] == "Visa Platinum":
-        return acc_info[14:]
-    elif acc_info[0:9] == "Visa Gold":
-        return acc_info[10:]
+    if acc_info[0:4] == "Счет":
+        slice_num = int(acc_info[5:])
+        masked = get_mask_account(slice_num)
+        return f"Счет {masked}"
     else:
-        return f"Счет **{acc_info[-4:]}"
+        slice_num = int(acc_info[-16:])
+        masked = get_mask_card_number(slice_num)
+        return f"{acc_info[0:-16]}{masked}"
 
 
 def get_date(date_old: str) -> str:
