db9e84362ab91a67c0bcb4a5230f2b67aaae81db
NikitaGamza <gamza-ns@mail.ru>
2026-02-20 06:40:06 +0400

    initiated sort_by_date function

--- src/processing.py
+++ src/processing.py
@@ -1,2 +1,5 @@
-def filter_by_state(proc: list[dict[str, str, str]], state='EXECUTED') -> list[dict[str, str, str]]:
+def filter_by_state(proc: list[dict[int, str, str]], state='EXECUTED') -> list[dict[int, str, str]]:
+    return proc
+
+def sort_by_date(proc: list[dict[int, str, str]], sequence=False) -> list[dict[int, str, str]]:
     return proc
