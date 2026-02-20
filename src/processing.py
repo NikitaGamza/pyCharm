ee46a4ececc031b60e6b6bb317792e2ef38cd9b8
NikitaGamza <gamza-ns@mail.ru>
2026-02-21 03:24:21 +0400

    sort_by_date is done

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,5 @@
 def filter_by_state(proc: list[dict], state='EXECUTED') -> list[dict]:
     return [item for item in proc if item.get('state') == state]
 
-def sort_by_date(proc: list[dict[int, str, str]], sequence=False) -> list[dict[int, str, str]]:
-    return proc
+def sort_by_date(proc: list[dict], sequence=False) -> list[dict]:
+    return sorted(proc, key=lambda x: x['date'], reverse=sequence)
