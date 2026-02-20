1482143d1adeae540a7b5460ee06b095ccf512fc
NikitaGamza <gamza-ns@mail.ru>
2026-02-21 03:59:26 +0400

    fix: literals

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,6 @@
-def filter_by_state(proc: list[dict], state='EXECUTED') -> list[dict]:
+def filter_by_state(proc: list[dict], state: str = 'EXECUTED') -> list[dict]:
     return [item for item in proc if item.get('state') == state]
 
-def sort_by_date(proc: list[dict], sequence=False) -> list[dict]:
+
+def sort_by_date(proc: list[dict], sequence: bool = False) -> list[dict]:
     return sorted(proc, key=lambda x: x['date'], reverse=sequence)
