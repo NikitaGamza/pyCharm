139159aae7de554e3dc84b6bb1218cdd6fac4f9a
NikitaGamza <gamza-ns@mail.ru>
2026-02-21 03:18:44 +0400

    fix: filter_by_state testing

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from src.masks import get_mask_account, get_mask_card_number
 from src.widget import get_date, mask_account_card
+from src.processing import filter_by_state
 
 get_mask_card_number(2222222222222222)
 get_mask_account(22222222222222221234)
-print(mask_account_card("Maestro 7158300734726758"))
-print(get_date("2024-03-11T02:26:18.671407"))
+# print(mask_account_card("Maestro 7158300734726758"))
+# print(get_date("2024-03-11T02:26:18.671407"))
+print(filter_by_state([{'id': 414288290, 'state': 'CANCELLED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]))
--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,5 @@
-def filter_by_state(proc: list[dict[int, str, str]], state='EXECUTED') -> list[dict[int, str, str]]:
-    return [item for item in proc if item.get(state) == state]
+def filter_by_state(proc: list[dict], state='EXECUTED') -> list[dict]:
+    return [item for item in proc if item.get('state') == state]
 
 def sort_by_date(proc: list[dict[int, str, str]], sequence=False) -> list[dict[int, str, str]]:
     return proc
