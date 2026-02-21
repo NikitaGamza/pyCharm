36a118b403baebf80c61e82cb20d6864500071bc
NikitaGamza <gamza-ns@mail.ru>
2026-02-21 06:02:03 +0400

    fix: processing typing

--- src/processing.py
+++ src/processing.py
@@ -1,8 +1,11 @@
-def filter_by_state(proc: list[dict], state: str = "EXECUTED") -> list[dict]:
+from typing import List, Dict, Union
+
+
+def filter_by_state(proc: List[Dict[str, Union[str | int]]], state: str = "EXECUTED") -> List[Dict[str, Union[str | int]]]:
     """Функция фильтрации по состоянию"""
     return [item for item in proc if item.get("state") == state]
 
 
-def sort_by_date(proc: list[dict], sequence: bool = True) -> list[dict]:
+def sort_by_date(proc: List[Dict[str, Union[str | int]]], sequence: bool = True) -> List[Dict[str, Union[str | int]]]:
     """Функция сортировки по дате"""
     return sorted(proc, key=lambda x: x["date"], reverse=sequence)
