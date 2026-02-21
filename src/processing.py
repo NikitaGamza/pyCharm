e7c83bb8e7e5f040cdd65213040c627c7140f7e3
NikitaGamza <gamza-ns@mail.ru>
2026-02-21 04:02:04 +0400

    fix: functions descriptions

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,8 @@
 def filter_by_state(proc: list[dict], state: str = 'EXECUTED') -> list[dict]:
+    '''Функция фильтрации по состоянию'''
     return [item for item in proc if item.get('state') == state]
 
 
 def sort_by_date(proc: list[dict], sequence: bool = False) -> list[dict]:
+    '''Функция сортировки по дате'''
     return sorted(proc, key=lambda x: x['date'], reverse=sequence)
