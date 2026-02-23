ae1a94094cde9a1ad267bc0db558095e5f5a55f0
NikitaGamza <gamza-ns@mail.ru>
2026-02-23 08:22:37 +0400

    fix: removed Union

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from src.masks import get_mask_account, get_mask_card_number
-
 # from src.widget import get_date, mask_account_card
 from src.processing import filter_by_state, sort_by_date
 
--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,11 @@
-from typing import List, Dict, Union
+from typing import Dict, List
 
 
-def filter_by_state(proc: List[Dict[str, Union[str | int]]],
-                    state: str = "EXECUTED") -> List[Dict[str, Union[str | int]]]:
+def filter_by_state(proc: List[Dict[str, str | int]], state: str = "EXECUTED") -> List[Dict[str, str | int]]:
     """Функция фильтрации по состоянию"""
-    return [item for item in proc if item .get("state") == state]
+    return [item for item in proc if item.get("state") == state]
 
 
-def sort_by_date(proc: List[Dict[str, Union[str | int]]], sequence: bool = False) -> List[Dict[str, Union[str | int]]]:
+def sort_by_date(proc: List[Dict[str, str | int]], sequence: bool = False) -> List[Dict[str, str | int]]:
     """Функция сортировки по дате"""
     return sorted(proc, key=lambda x: x["date"][:11], reverse=sequence)
