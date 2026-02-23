6b6068003c4b6dc15134342218bc5ffa0c6683c2
NikitaGamza <gamza-ns@mail.ru>
2026-02-23 08:25:33 +0400

    fix: renamed values

--- src/processing.py
+++ src/processing.py
@@ -1,11 +1,11 @@
 from typing import Dict, List
 
 
-def filter_by_state(proc: List[Dict[str, str | int]], state: str = "EXECUTED") -> List[Dict[str, str | int]]:
+def filter_by_state(operations: List[Dict[str, str | int]], state: str = "EXECUTED") -> List[Dict[str, str | int]]:
     """Функция фильтрации по состоянию"""
-    return [item for item in proc if item.get("state") == state]
+    return [item for item in operations if item.get("state") == state]
 
 
-def sort_by_date(proc: List[Dict[str, str | int]], sequence: bool = False) -> List[Dict[str, str | int]]:
+def sort_by_date(operations: List[Dict[str, str | int]], sequence: bool = False) -> List[Dict[str, str | int]]:
     """Функция сортировки по дате"""
-    return sorted(proc, key=lambda x: x["date"][:11], reverse=sequence)
+    return sorted(operations, key=lambda x: x["date"][:11], reverse=sequence)
