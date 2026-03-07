a466523d4b03f185c4999a634d68b30a71b94b17
NikitaGamza <gamza-ns@mail.ru>
2026-03-07 10:26:02 +0400

    feat: feat additional tests for filter_by_state

--- tests/test_filter_by_state.py
+++ tests/test_filter_by_state.py
@@ -7,3 +7,21 @@ def test_filter_by_state():
             {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
         ], state="SOMESTATE"
         ) == []
+
+    assert filter_by_state([
+        {"id": 414288290, "state": "CANCELLED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ],
+    ) == [{"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}]
+
+    assert filter_by_state([
+        {"id": 414288290, "state": "CANCELLED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ], state="EXECUTED"
+    ) == [{"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}]
+
+    assert filter_by_state([
+        {"id": 414288290, "state": "CANCELLED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ], state="CANCELLED"
+    ) == [{"id": 414288290, "state": "CANCELLED", "date": "2019-07-03T18:35:29.512364"}]
