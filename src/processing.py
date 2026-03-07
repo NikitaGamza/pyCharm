9c985fb6e97ed79b8c92ead1cac91800f0a8b90c
NikitaGamza <gamza-ns@mail.ru>
2026-03-07 09:50:09 +0400

    fix: type Any

--- src/processing.py
+++ src/processing.py
@@ -1,11 +1,11 @@
-from typing import Dict, List
+from typing import Dict, List, Any
 
 
-def filter_by_state(operations: List[Dict[str, str | int]], state: str = "EXECUTED") -> List[Dict[str, str | int]]:
+def filter_by_state(operations: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
     """Функция фильтрации по состоянию"""
     return [item for item in operations if item.get("state") == state]
 
 
-def sort_by_date(operations: List[Dict[str, str]], reverse : bool = True) -> List[Dict[str, str]]:
+def sort_by_date(operations: List[Dict[str, Any]], reverse : bool = True) -> List[Dict[str, Any]]:
     """Функция сортировки по дате"""
     return sorted(operations, key=lambda item : item ["date"][0:11], reverse=reverse )
