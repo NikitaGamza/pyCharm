25aeaa4010f98cda34925747dbc5c1f1a7cc575a
NikitaGamza <gamza-ns@mail.ru>
2026-03-07 10:23:32 +0400

    feat: feat additional tests + linters

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Any
+from typing import Any, Dict, List
 
 
 def filter_by_state(operations: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
@@ -6,6 +6,6 @@ def filter_by_state(operations: List[Dict[str, Any]], state: str = "EXECUTED") -
     return [item for item in operations if item.get("state") == state]
 
 
-def sort_by_date(operations: List[Dict[str, Any]], reverse : bool = True) -> List[Dict[str, Any]]:
+def sort_by_date(operations: List[Dict[str, Any]], reverse: bool = True) -> List[Dict[str, Any]]:
     """Функция сортировки по дате"""
-    return sorted(operations, key=lambda item : item ["date"][0:11], reverse=reverse )
+    return sorted(operations, key=lambda item: item["date"][0:11], reverse=reverse)
