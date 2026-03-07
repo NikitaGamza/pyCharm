c49f4d32bd0d16291b25df74bd4c42af873165df
NikitaGamza <gamza-ns@mail.ru>
2026-03-07 10:12:45 +0400

    feat: test for filter_by_state

---
+++ tests/test_filter_by_state.py
@@ -0,0 +1,9 @@
+from src.processing import filter_by_state
+
+
+def test_filter_by_state():
+    assert filter_by_state([
+            {"id": 414288290, "state": "CANCELLED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        ],
+        ) == [{"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}]
