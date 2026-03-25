e11968ee84735d874a8672057dfd44bc296b95d4
NikitaGamza <gamza-ns@mail.ru>
2026-03-25 13:10:08 +0400

    fix: linters

--- src/generators.py
+++ src/generators.py
@@ -1,7 +1,8 @@
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Generator
 
-def filter_by_currency(transactions: List[Dict[str, Any]], currency: str) -> List[Dict[str, Any]]:
-    '''Генератор фильтрации по заданной валюте'''
+
+def filter_by_currency(transactions: List[Dict[str, Any]], currency: str) -> Generator:
+    """Генератор фильтрации по заданной валюте"""
     for transaction in transactions:
         if transaction.get("operationAmount", {}).get("currency", {}).get("code") == currency:
             yield transaction
