a9083364bc3987f7601388cee09eb12e4a28e554
NikitaGamza <gamza-ns@mail.ru>
2026-03-25 13:48:55 +0400

    feat: transaction_descriptions function

--- src/generators.py
+++ src/generators.py
@@ -6,3 +6,8 @@ def filter_by_currency(transactions: List[Dict[str, Any]], currency: str) -> Gen
     for transaction in transactions:
         if transaction.get("operationAmount", {}).get("currency", {}).get("code") == currency:
             yield transaction
+
+
+def transaction_descriptions(transactions: List[Dict[str, Any]]) -> Generator:
+    for transaction in transactions:
+        yield transaction["description"]
