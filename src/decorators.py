be441ef2093b0ea419dcaeafab7ff034134db08e
NikitaGamza <gamza-ns@mail.ru>
2026-04-12 13:56:37 +0400

    fix: linters

--- src/decorators.py
+++ src/decorators.py
@@ -1,16 +1,17 @@
 from functools import wraps
+from typing import Any
 
 
-def log(filename=None):
+def log(filename: Any = None) -> Any:
     """
       Декоратор логирования  начала и конца выполнения функции, ее результаты и возникшие ошибки.
      Принимать необязательный аргумент 'filename', определяющий,
     куда будут записываться логи (в файл или в консоль)
     """
 
-    def wrapper(function):
+    def wrapper(function: Any) -> Any:
         @wraps(function)
-        def inner(*args, **kwargs):
+        def inner(*args: Any, **kwargs: Any) -> Any:
             try:
                 result = function(*args, **kwargs)
                 log_message = f"{function.__name__} ok\n"
