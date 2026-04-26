11a0dda7805dc2c8c8705deeb5d75d9fc49d5c9c
NikitaGamza <gamza-ns@mail.ru>
2026-04-26 18:14:55 +0400

    fix: test

--- tests/test_conversion_amount.py
+++ tests/test_conversion_amount.py
@@ -5,8 +5,8 @@ from unittest.mock import patch, Mock
 def test_conversion_amount_success():
     mock_response = Mock()
     mock_response.status_code = 200
-    mock_response.json.return_value = {"result": 10}
+    mock_response.json.return_value = {"result": 882.42}
 
     with patch("requests.get", return_value=mock_response):
-        result = conversion_amount("USD")
-        assert result == 10
+        result = conversion_amount(10, "EUR", "RUB")
+        assert result == 882.42
