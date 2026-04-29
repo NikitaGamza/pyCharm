671c7793092966bc52ffc66a7abcab96d9b2dcf3
NikitaGamza <gamza-ns@mail.ru>
2026-04-30 03:19:03 +0400

    fix: tests

--- src/masks.py
+++ src/masks.py
@@ -3,7 +3,7 @@ import logging
 from black.comments import Union
 
 logger = logging.getLogger("masks")
-logger.setLevel(logging.ERROR)
+logger.setLevel(logging.INFO)
 file_handler = logging.FileHandler("logs/masks.log", "w", encoding="utf-8")
 file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 file_handler.setFormatter(file_formatter)
