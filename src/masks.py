c957c99ab211af2b3a63062acf45b6de667cd49c
NikitaGamza <gamza-ns@mail.ru>
2026-04-30 03:10:56 +0400

    fix: linters

--- src/masks.py
+++ src/masks.py
@@ -3,7 +3,7 @@ import logging
 from black.comments import Union
 
 logger = logging.getLogger("masks")
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.ERROR)
 file_handler = logging.FileHandler("logs/masks.log", "w", encoding="utf-8")
 file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 file_handler.setFormatter(file_formatter)
@@ -12,7 +12,7 @@ logger.addHandler(file_handler)
 
 def get_mask_card_number(cart_number: int) -> Union[str, None]:
     """Функция маскировки карты"""
-    logger.debug(f"Попытка маскировки карты")
+    logger.debug("Попытка маскировки карты")
     if len(str(cart_number)) < 16 or len(str(cart_number)) > 16:
         print("Некорректный ввод")
         logger.error("Некорректный ввод")
@@ -30,7 +30,7 @@ def get_mask_card_number(cart_number: int) -> Union[str, None]:
 
 def get_mask_account(acc_number: int) -> Union[str, None]:
     """Функция маскировки аккаунта"""
-    logger.debug(f"Попытка маскировки аккаунта")
+    logger.debug("Попытка маскировки аккаунта")
     if len(str(acc_number)) < 20 or len(str(acc_number)) > 20:
         print("Некорректный ввод")
         logger.error("Некорректный ввод")
