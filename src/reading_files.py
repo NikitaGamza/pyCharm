c59a0a58a95f2fa6b8d7e092a7e6ed783038477b
NikitaGamza <gamza-ns@mail.ru>
2026-05-05 05:56:30 +0400

    feat: reading csv ans excel files

---
+++ src/reading_files.py
@@ -0,0 +1,33 @@
+import os
+from typing import Any
+import pandas as pd
+import csv
+
+ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
+PATH_TO_FILE = os.path.join(ROOT_DIR, "data", "transactions_excel.xlsx")
+PATH_TO_FILE_CSV = os.path.join(ROOT_DIR, "data", "transactions.csv")
+
+def read_excel_file(address: str) -> list[Any]|str:
+    """Чтоение финансовых операций excel файла"""
+    try:
+        pd_file = pd.read_excel(address)
+        result = pd_file.fillna("").to_dict(orient="records")
+    except FileNotFoundError:
+        return "File not found"
+    except Exception as e:
+        return f"Error: {e}"
+    return result
+
+def read_csv_file(address: str) -> list[Any]|str:
+    """Чтение финансовых операций csv файла"""
+    transactions = []
+    try:
+        with open(address, mode='r', encoding='utf-8') as file:
+            reader = csv.DictReader(file, delimiter=';')
+            for row in reader:
+                transactions.append(dict(row))
+    except FileNotFoundError:
+        return "File not found"
+    except Exception as e:
+        return f"Error: {e}"
+    return transactions
