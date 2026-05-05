d84bbdb24862415ef90fe0a8bda16f456cf53473
NikitaGamza <gamza-ns@mail.ru>
2026-05-05 06:09:26 +0400

    fix: linters

--- src/reading_files.py
+++ src/reading_files.py
@@ -1,13 +1,15 @@
+import csv
 import os
 from typing import Any
+
 import pandas as pd
-import csv
 
 ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
 PATH_TO_FILE = os.path.join(ROOT_DIR, "data", "transactions_excel.xlsx")
 PATH_TO_FILE_CSV = os.path.join(ROOT_DIR, "data", "transactions.csv")
 
-def read_excel_file(address: str) -> list[Any]|str:
+
+def read_excel_file(address: str) -> Any:
     """Чтоение финансовых операций excel файла"""
     try:
         pd_file = pd.read_excel(address)
@@ -18,12 +20,13 @@ def read_excel_file(address: str) -> list[Any]|str:
         return f"Error: {e}"
     return result
 
-def read_csv_file(address: str) -> list[Any]|str:
+
+def read_csv_file(address: str) -> Any:
     """Чтение финансовых операций csv файла"""
     transactions = []
     try:
-        with open(address, mode='r', encoding='utf-8') as file:
-            reader = csv.DictReader(file, delimiter=';')
+        with open(address, mode="r", encoding="utf-8") as file:
+            reader = csv.DictReader(file, delimiter=";")
             for row in reader:
                 transactions.append(dict(row))
     except FileNotFoundError:
