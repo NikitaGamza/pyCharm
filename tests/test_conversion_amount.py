a91cb6c5139629d3cc863012c467a390b109c060
NikitaGamza <gamza-ns@mail.ru>
2026-05-06 03:40:02 +0400

    fix: description test_conversion_amount.py in comment

--- tests/test_conversion_amount.py
+++ tests/test_conversion_amount.py
@@ -5,8 +5,8 @@ from unittest.mock import patch, Mock
 def test_conversion_amount_success():
     mock_response = Mock()
     mock_response.status_code = 200
-    mock_response.json.return_value = {"result": 882.42}
+    mock_response.json.return_value = {"result": 884.79} #Результат тестирования меняется в зависимости от курса валют
 
     with patch("requests.get", return_value=mock_response):
         result = conversion_amount(10, "EUR", "RUB")
-        assert result == 882.42
+        assert result == 884.79
