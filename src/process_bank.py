ccb90af6d6767b13931c3a34a106b8abcf13d101
NikitaGamza <gamza-ns@mail.ru>
2026-05-25 01:26:53 +0400

    fix: linters

--- src/process_bank.py
+++ src/process_bank.py
@@ -1,14 +1,13 @@
 import re
-from typing import List, Dict
+from typing import Dict, List
 
 
-def process_bank_search(data:List[Dict], search:str)->List[Dict]:
-    '''Функция поиска по строке'''
-    #автоматически экранируем все специальные символы (метасимволы) в строке через re.escape()
+def process_bank_search(data: List[Dict], search: str) -> List[Dict]:
+    """Функция поиска по строке"""
+    # автоматически экранируем все специальные символы (метасимволы) в строке через re.escape()
     pattern = re.compile(re.escape(search), re.IGNORECASE)
 
     result = [
-        operation for operation in data
-        if 'description' in operation and pattern.search(operation['description'])
+        operation for operation in data if "description" in operation and pattern.search(operation["description"])
     ]
     return result
