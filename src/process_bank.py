ed77d5f18ef56a1366ab1d7c299edb55ee41449f
NikitaGamza <gamza-ns@mail.ru>
2026-05-25 01:43:17 +0400

    feat: process_bank_operations

--- src/process_bank.py
+++ src/process_bank.py
@@ -1,5 +1,6 @@
 import re
 from typing import Dict, List
+from collections import Counter
 
 
 def process_bank_search(data: List[Dict], search: str) -> List[Dict]:
@@ -11,3 +12,11 @@ def process_bank_search(data: List[Dict], search: str) -> List[Dict]:
         operation for operation in data if "description" in operation and pattern.search(operation["description"])
     ]
     return result
+
+def process_bank_operations(data: List[Dict], categories: list) -> dict:
+    """
+       Функция вывода количества операций в каждой категории.
+    """
+    descriptions = [transaction['description'] for transaction in data]
+    filtered_descriptions = [desc for desc in descriptions if desc in categories]
+    return dict(Counter(filtered_descriptions))
